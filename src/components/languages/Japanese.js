830b6c0c64b060bdfd1a4a1129812c4e2b8afaea
Joshua Gaucin <joshuagaucin01@gmail.com>
2020-12-21 21:03:00 -0600

    corrected japanese

--- src/components/languages/Japanese.js
+++ src/components/languages/Japanese.js
@@ -6,10 +6,10 @@ class Japanese extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1>こんにちわ世界！</h1>
+        <h1>こんにちは世界！</h1>
 
         <p className="intro">
-          名前がジョシュアガウシアンです。言語が大好きなソフトウェアエンジニアです。人間またはコンピューター。ロゼッタストーンで働きたい
+          私の名前はジョシュアガウシアンです。私はソフトウェアエンジニアで、語学、人間、コンピューターが大好きです。ロゼッタストーンで働きたい
         </p>
         <p>この楽しいクイズをお楽しみください</p>
         <JapaneseQuestions />
